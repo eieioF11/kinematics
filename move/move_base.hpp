0e49241165c41861a5bce05f15adb3d1aba4ad08
f11 <eieioeiji0501@gmail.com>
2024-03-16 18:16:46 +0900

    [fixed]ミス修正

--- move/move_base.hpp
+++ move/move_base.hpp
@@ -1,10 +1,10 @@
 #pragma once
+#include "../math_util.hpp"
 #include <cmath>
 #include <iostream>
 #include <numeric>
 #include <ratio>
 #include <vector>
-#include "../math_util.hpp"
 
 namespace kinematics {
 
@@ -22,8 +22,8 @@ namespace kinematics {
     using value_type = FLOATING_TYPE;
 
   public:
-    MoveBase();
-    MoveBase();
+    MoveBase() {}
+    MoveBase(value_type r, value_type l,value_type w,value_type h) : R(r), L(l), WIDTH(w), HEIGHT(h) {}
 
     /**
      * @brief 3次元の速度入力
@@ -43,27 +43,25 @@ namespace kinematics {
      * @param vy [m/s]
      * @param angular_z [rad/s]
      */
-    void move(value_type vx, value_type vy, value_type angular_z) { move(vx, vy, angular_z); }
+    void move(value_type vx, value_type vy, value_type angular_z) { move(vx, vy, 0., 0., 0., angular_z); }
     /**
      * @brief 停止動作
      *
      */
-    virtual void stop() const {
-      move(0, 0, 0, 0, 0, 0);
-    }
+    virtual void stop() { move(0., 0., 0., 0., 0., 0.); }
 
     /**
      * @brief 車輪の速度を取得
      *
      * @return std::vector<value_type>
      */
-    std::vector<value_type> get_wheel_speeds() const { return wheel_speeds; }
+    std::vector<value_type> get_wheel_speeds() const { return wheel_speeds_; }
 
   protected:
-    std::vector<value_type> wheel_speeds; // [rps]
+    std::vector<value_type> wheel_speeds_; // [rps]
     // robot parameter
-    value_type R; //wheel radius [m]
-    value_type L; //half intervals between wheels [m]
+    value_type R; // wheel radius [m]
+    value_type L; // half intervals between wheels [m]
     value_type WIDTH;
     value_type HEIGHT;
   };
--- move/two_wheels.hpp
+++ move/two_wheels.hpp
@@ -19,17 +19,17 @@ namespace kinematics {
   using TwoWheelsd = TwoWheels<double>;
 
   template <typename FLOATING_TYPE>
-  class TwoWheels : public MoveBase {
+  class TwoWheels : public MoveBase<FLOATING_TYPE> {
     using value_type = FLOATING_TYPE;
+
   private:
     value_type inv_r_;
+
   public:
-    Two_wheels(){}
-    Two_wheels(value_type r,value_type l){// r:wheel radius[m], l:diameter[m]
-      R = r;
-      L = l;
-      inv_r_ = 1./R;
-      wheel_speeds.resize(2);
+    TwoWheels() : MoveBase<value_type>() {}
+    TwoWheels(value_type r, value_type l) : MoveBase<value_type>(r, l, 0., 0.) { // r:wheel radius[m], l:diameter[m]
+      inv_r_ = 1. / r;
+      this->wheel_speeds_.resize(2);
     }
 
     /**
@@ -43,18 +43,18 @@ namespace kinematics {
      * @param angular_z [rad/s]
      */
     void move(value_type vx, value_type vy, value_type vz, value_type angular_x, value_type angular_y, value_type angular_z) {
-      wheel_speeds[0] = (vx*inv_r_-(angular_z*L*inv_r_))*RADPS_TO_RPS;
-      wheel_speeds[1] = (vx*inv_r_+(angular_z*L*inv_r_))*RADPS_TO_RPS;
+      this->wheel_speeds_[0] = (vx * inv_r_ - (angular_z * this->L * inv_r_)) * math_util::constants::RADPS_TO_RPS;
+      this->wheel_speeds_[1] = (vx * inv_r_ + (angular_z * this->L * inv_r_)) * math_util::constants::RADPS_TO_RPS;
     }
   };
-/*---------------------
-left wheel(l):0
-right wheel(r):1
-  |-L-|
-l[0]--|--[1]r
- x
- |
-_|____y
-o|   
-----------------------*/
+  /*---------------------
+  left wheel(l):0
+  right wheel(r):1
+    |-L-|
+  l[0]--|--[1]r
+   x
+   |
+  _|____y
+  o|
+  ----------------------*/
 } // namespace kinematics
